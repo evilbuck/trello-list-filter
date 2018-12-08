62b6ee8b86872fa558a6cb1307e9c43a17ac1af7
Buckley Robinson <buckley@brightbytes.net>
2018-12-08 10:53:42 -0800

    Feature: Autocomplete added. Remembers unique search expressions. Limited to 7 items for UX.

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,8 +1,63 @@
-const $filter = $('<div id="evil-list-filter">' +
-                '<input class="header-search-input" type="text" placeholder="Filter Lists"/></div>');
+const storageKey = 'trello-list-filter:queryString';
+
+function getCurrentItems() {
+  let currentItems;
+  try {
+    currentItems = JSON.parse(localStorage.getItem(storageKey));
+    if (!(currentItems instanceof Array)) {
+      throw new Error('items not an Array');
+    }
+  } catch(error) {
+    currentItems = [];
+  }
+
+  return currentItems;
+}
+
+function saveQuery(expr) {
+  // don't save a lack of value
+  if (expr === '' || expr === null || expr === undefined) {
+    return;
+  }
+  let currentItems = getCurrentItems();
+  currentItems.unshift(expr);
+
+  let updatedItems = currentItems.reduce((uniqueItems, item) => {
+    if (!uniqueItems.includes(item)) {
+      uniqueItems.push(item);
+    }
+
+    return uniqueItems;
+  }, []).slice(0, 7);
+
+  localStorage.setItem(storageKey, JSON.stringify(updatedItems));
+  updateAutoCompleteOptions();
+}
+
+function updateAutoCompleteOptions() {
+  let currentItems = getCurrentItems();
+  let autocompleteList = document.getElementById('trello-list-filter-autocomplete');
+  autocompleteList.innerHTML = '';
+  currentItems.forEach((item) => {
+    let option = document.createElement('option');
+    option.innerText = item;
+    autocompleteList.appendChild(option);
+  });
+}
+
+const $filter = $(`
+  <div id="evil-list-filter">' +
+    '<input class="header-search-input" type="text" placeholder="Filter Lists" autocomplete=off
+        list="trello-list-filter-autocomplete"/>
+    <datalist id="trello-list-filter-autocomplete"></datalist>
+  </div>`
+);
+
+let searchTimeout;
 $filter.find('input').on('keyup', function() {
   let $input = $(this);
-  let query = new RegExp($input.val(), 'i');
+  let queryString = $input.val();
+  let query = new RegExp(queryString, 'i');
   $('#board > .list-wrapper').each(function(el, i) {
     let $list = $(this);
 
@@ -13,6 +68,11 @@ $filter.find('input').on('keyup', function() {
       $list.hide();
     }
   });
+
+  try {
+    clearTimeout(searchTimeout);
+  } catch(error) {}
+  searchTimeout = setTimeout(() => saveQuery(queryString), 750);
 });
 
 const filterInput = $filter.find('input').get(0);
@@ -23,6 +83,7 @@ filterInput.addEventListener('focus', (event) => {
 const domObserver = new MutationObserver(function(mutations) {
   if (!$('#header').find($filter).length) {
     $filter.insertAfter('#header .header-search');
+    updateAutoCompleteOptions();
   }
 });
 
